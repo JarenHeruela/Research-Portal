3069adbfbaf2564cab68d25cf375318360f7a6fd
JarenHeruela <jarenloydheruela@gmail.com>
2024-01-11 22:48:33 +0800

    Initial

---
+++ research-portal/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+      </header>
+      <body>
+        <h1>Welcome to the Coding Dojo!</h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
